6c88714e100bb906c3305d4fe07ae0799cad19f9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-02 11:19:47 +0100

    Importerte RDF filer sendes nå gjennom Jena før de skrives til fil slik at de er formattert.

--- lib/kompetansesok/importerer.rb
+++ lib/kompetansesok/importerer.rb
@@ -1,15 +1,55 @@
+raise "Denne koden krever JRuby" unless defined?(JRUBY_VERSION)
 require 'fileutils'
 require 'open-uri'
 require 'rubygems'
 require 'feed_tools'
-JRuby.objectspace = true if defined?(JRUBY_VERSION)
+require 'java'
+Dir[File.dirname(__FILE__) + '/../ext/jena-2.5.6/*.jar'].each do |jar|
+  require jar
+end
+JRuby.objectspace = true
 
 module Kompetansesok
+  class Jena # TODO: flytt til egen klasse. Enhetstest med RSpec!
+    GREP_NS = 'http://psi.udir.no/ontologi/'
+
+    def initialize
+      @model = com.hp.hpl.jena.rdf.model.ModelFactory.createDefaultModel
+      @kompetansemaal_property = @model.getProperty(GREP_NS, 'kompetansemaalsett_har_kompetansemaal')
+      @title_property          = com.hp.hpl.jena.vocabulary.DC_11.title
+    end
+    
+    def les_rdf_fil(rdf_fil)
+      f = com.hp.hpl.jena.util.FileManager.get.open(rdf_fil)
+      @model.read(f, "")
+    end
+
+    def les_rdf_url(rdf_url)
+      @model.read(rdf_url, "")
+    end
+    
+    def write(out)
+      @model.write(out.to_outputstream)
+    end
+    
+    # Returnerer alle kompetansemaal som Array av Hash. Hver Hash er et key-value par med primitive verdier.
+    def kompetansemaal
+      @model.listResourcesWithProperty(@kompetansemaal_property).map do |km|
+        {
+          :title => km.getProperty(@title_property).string
+        }
+      end
+    end
+  end
+
   class Importerer
     ATOM_URL = 'http://lkt.udir.no/eksport/rdf.mvc/laereplan'
     
-    def initialize(import_dir = Rails.root + '/tmp/import')
+    # Instansierer ny importerer som henter/leser RDF filer til/fra +import_dir+. Hvis
+    # +out+ er spesifisert logges enkel status hit.
+    def initialize(import_dir, out=nil)
       @import_dir = import_dir
+      @out = out
     end
     
     def slett_filer
@@ -23,41 +63,40 @@ module Kompetansesok
     def importer_til_fil(n=nil)
       FileUtils.mkdir_p(@import_dir)
 
+      @out.puts("Leser Atom feed...") if @out
       feed = FeedTools::Feed.open(ATOM_URL)
       entries = n.nil? ? feed.items : feed.items[0...n]
+      @out.puts("Importerer #{entries.length} RDF filer...") if @out
       entries.each do |entry|
+        @out.write('.') if @out
         if entry.guid =~ /uuid:(.*);id=(.*)/
           id = $2
-          open(entry.link) do |rdf|
-            File.open(File.join(@import_dir, "#{id}.rdf"), 'wb') do |f|
-              f.write(rdf.read)
-            end
+          jena = Jena.new
+          jena.les_rdf_url(entry.link)
+
+          File.open(File.join(@import_dir, "#{id}.rdf"), 'wb') do |f|
+            jena.write(f)
           end
         else
           raise "Feil guid format på feed entry: #{entry.guid}"
         end
       end
+      @out.puts if @out
     end
 
     def importer_til_db(n=nil)
-      require 'java'
-      Dir[File.dirname(__FILE__) + '/../ext/jena-2.5.6/*.jar'].each do |jar|
-        require jar
+      jena = Jena.new
+      
+      fil_array = n.nil? ? filer : filer[0...n]
+      @out.puts("Leser #{fil_array.length} RDF filer...") if @out
+      fil_array.each do |rdf_fil|
+        @out.write('.') if @out
+        jena.les_rdf_fil(rdf_fil)
       end
-
-      model = com.hp.hpl.jena.rdf.model.ModelFactory.createDefaultModel
-
-      grep = 'http://psi.udir.no/ontologi/'
-      kompetansemaal_property = model.getProperty(grep, 'kompetansemaalsett_har_kompetansemaal')
-      title_property          = com.hp.hpl.jena.vocabulary.DC_11.title
-
-      fa = n.nil? ? filer : filer[0...n]
-      fa.each do |rdf|
-        fm = com.hp.hpl.jena.util.FileManager.get.open(rdf)
-        model.read(fm, "")
-      end
-      model.listResourcesWithProperty(kompetansemaal_property).each do |res|
-        #puts res.getProperty(title_property).string
+      @out.puts if @out
+      
+      jena.kompetansemaal.each do |km|
+        @out.puts km[:title] if @out
       end
     end
 
