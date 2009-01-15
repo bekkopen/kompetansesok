d0ecbcf6d27f682ae8f6e49a02694a35e5c8a846
aslakjo <aslakjo@bekk.no>
2009-01-15 12:22:54 +0100

    refactorerte ValgteKompetansemaal.js til et objekt

--- public/javascripts/Valgtekompetansemaal.js
+++ public/javascripts/Valgtekompetansemaal.js
@@ -1,58 +1,88 @@
-function setSelectedKompetansemaal()
-{
-    valgte_kompetansemaal = getSelectedKompetansemaal();
-    valgte_data = retriveDataFromGrid(valgte_kompetansemaal);
+function ValgteKompetansemaal(){
+    this.showId = true;
+    this.showKode = false;
+    this.showPsi = false;
 
-    valgte_grid = document.getElementById("valgte_kompetansemaal");
-    valgte_grid.data = valgte_data;
+    this.tekst_id = "valgte_tekst";
+    this.data = [];
 
-    showValgteKompetansemaal();
+    
 
-};
+    this.displayValgteKompetansemaal = function()
+    {
+        valgte_window = document.getElementById(this.tekst_id);
+        
+        valgte_window.innerHTML = "";
+        for(i in this.data){
+            valgte_window.innerHTML = valgte_window.innerHTML + "\n" + this.formatedKompetansemaalLinje(this.data[i]);
+        }
+    };
 
-function showValgteKompetansemaal()
-{
-    valgte_window = document.getElementById("valgte_tekst");
-    valgte_grid = document.getElementById("valgte_kompetansemaal");
+    this.retriveDataFromExtGrid = function(gridData)
+    {
+        valgte_data = []
+        for(i in gridData){
+            data = gridData[i].data;
+            valgte_data[i] = data;
+        }
+        this.data = valgte_data;
+    };
 
-    data = valgte_grid.data
-    valgte_window.innerHTML = "";
-    for(i in data){
-        valgte_window.innerHTML = valgte_window.innerHTML + "\n" + formatedKompetansemaalLinje(data[i])
-    }
-}
+    this.formatedKompetansemaalLinje = function(gridData){
+        line = "";
 
-function formatedKompetansemaalLinje(gridData){
-    line = "";
-    valgte_grid = document.getElementById("valgte_kompetansemaal");
-    if(gridData == undefined){
-        return ""
+        if(gridData == undefined)
+            return line;
+
+        if(valgte_kontroller.showId == true){
+            line = this.prependComma(line, gridData['kompetansemaal_uuid']);
+        }
+
+        if(valgte_kontroller.showKode == true){
+            line = this.prependComma(line, gridData['kompetansemaal_kode']);
+        }
+
+        if(valgte_kontroller.showPsi == true){
+            line = this.prependComma(line, gridData['kompetansemaal_psi']);
+        }
+
+        return line;
+    };
+
+    this.prependComma = function(line, nextElement){
+        if(line.length > 0){
+            return line + "," + nextElement;
+        }
+        return nextElement;
+    };
+
+    this.toggleShowId=function(show)
+    {
+        this.showId = show;
+
+        importAndDisplaySelectedKompetansemaal();
     }
     
-    if(valgte_grid.showId == true){
-        line = line + gridData['kompetansemaal_uuid'];
-    }
 
-    return line;
 };
 
-function retriveDataFromGrid(gridData)
+function importAndDisplaySelectedKompetansemaal()
 {
-     valgte_data = []
-     for(i in gridData){
-        data = gridData[i].data;
-        valgte_data[i] = data;
-    }
-    return valgte_data
-};
-
-function toggleShowId(show)
-{
+    valgte_kompetansemaal = getSelectedKompetansemaal();
     valgte_grid = document.getElementById("valgte_kompetansemaal");
-    valgte_grid.showId = show;
+    valgte_grid.valgte_kompetansemaal.retriveDataFromExtGrid(valgte_kompetansemaal);
+    
 
-    setSelectedKompetansemaal();
-}
+    valgte_grid.valgte_kompetansemaal.displayValgteKompetansemaal();
+
+};
+
+
+
+
+
+
+
 
 
 
