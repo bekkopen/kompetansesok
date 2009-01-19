6ba2af68357d8039d6fd3fd33812e915149b1074
Øystein Ellingbø <ellingbo@gmail.com>
2009-01-19 09:24:56 +0100

    definert relasjon mellom fag og kompetansemålsett

--- app/models/fag.rb
+++ app/models/fag.rb
@@ -1,4 +1,6 @@
 class Fag < ActiveRecord::Base
+  has_and_belongs_to_many :kompetansemaalsett
+  
   def to_param
     uuid
   end
--- spec/models/fag_spec.rb
+++ spec/models/fag_spec.rb
@@ -12,4 +12,17 @@ describe Fag do
   it "should create a new instance given valid attributes" do
     Fag.create!(@valid_attributes)
   end
+  
+  describe "relationships" do
+    before :each do
+      @fag = Fag.new(@valid_attributes)
+    end
+    
+    it "should have kompetansemaalsett" do
+      kompetansemaalsett = Kompetansemaalsett.new
+      @fag.kompetansemaalsett << kompetansemaalsett
+      @fag.kompetansemaalsett.should == [kompetansemaalsett]
+    end
+   
+  end
 end
