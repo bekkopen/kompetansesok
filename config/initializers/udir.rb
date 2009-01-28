4ce68123bee51cc15308a82e8dddfb5bd4c45b04
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-28 19:45:09 +0100

    Fikset opp i en haug med tester som feilet

--- config/initializers/udir.rb
+++ config/initializers/udir.rb
@@ -1,5 +1,10 @@
+require 'fileutils'
+
 module Udir
   CONFIG = YAML.load_file(File.join(Rails.root, 'config/udir.yml'))
-  
+
   DB_DUMP_PATH = File.join(Rails.public_path, CONFIG['db_dumps']['path'])
+  FileUtils.mkdir_p(DB_DUMP_PATH) unless File.directory?(DB_DUMP_PATH)
+
+  DUMP_COUNT   = CONFIG['db_dumps']['count']
 end
--- lib/kompetansesok/db_eksport.rb
+++ lib/kompetansesok/db_eksport.rb
@@ -1,17 +1,14 @@
 module Kompetansesok
   class DbEksport < RunCommand
-
-    @@zip_path = File.join(Rails.root, 'public', 'db_dumps')
-
-    def self.db_name
+    def db_name
       ActiveRecord::Base.configurations[Rails.env]['database']
     end
-    
-    def self.db_user
+
+    def db_user
       ActiveRecord::Base.configurations[Rails.env]['username']
     end
 
-    def self.db_pass_prase
+    def db_pass_prase
       if ActiveRecord::Base.configurations[Rails.env]['password']
         "-p#{ActiveRecord::Base.configurations[Rails.env]['password']}"
       else
@@ -19,18 +16,14 @@ module Kompetansesok
       end
     end
 
-    def self.eksporter
+    def eksporter
       dump_filename = 'kompetansesok_mysql.dump'
       dump_filepath = File.join(Rails.root, 'tmp', dump_filename)
-      system("mysqldump -u#{db_user} #{db_name} #{db_pass_prase} > #{dump_filepath}")
-
-      if not File.exist?(@@zip_path)
-        File.makedirs(@@zip_path)
-      end
+      run_command("mysqldump -u#{db_user} #{db_name} #{db_pass_prase} > #{dump_filepath}")
 
       time_stamp = Time.now.to_datetime.strftime("%d-%m-%Y-%H:%M")
       zip_filename = "db_dump_#{time_stamp}.zip"
-      zip_savepath = File.join(@@zip_path, zip_filename)
+      zip_savepath = File.join(Udir::DB_DUMP_PATH, zip_filename)
 
       require 'zip/zipfilesystem'
       Zip::ZipFile.open(zip_savepath, Zip::ZipFile::CREATE) do |zipfile|
@@ -40,16 +33,9 @@ module Kompetansesok
       File.delete(dump_filepath)
     end
 
-    def self.cleanup
-      if not File.exist?(@@zip_path)
-        return
-      end
-
-      #TODO fetch this number from the configuration
-      keep_number_of_dumps = 3
-
+    def cleanup
       existing_dumps = {}
-      zip_dir = Dir.new(@@zip_path)
+      zip_dir = Dir.new(Udir::DB_DUMP_PATH)
       while(entry =  zip_dir.read)
         if entry =~ /db_dump_(.+)\.zip/
           time_stamp = Time.parse($1)
@@ -57,33 +43,33 @@ module Kompetansesok
         end
       end
 
-      old_dump_keys = existing_dumps.keys.sort.reverse.slice(keep_number_of_dumps, existing_dumps.keys.length)
+      old_dump_keys = existing_dumps.keys.sort.reverse.slice(Udir::DUMP_COUNT, existing_dumps.keys.length)
 
       if old_dump_keys
         old_dump_keys.each do |stamp_key|
-          File.delete(File.join(@@zip_path,existing_dumps[stamp_key]))
+          File.delete(File.join(Udir::DB_DUMP_PATH, existing_dumps[stamp_key]))
         end
       end
     end
 
-    def self.restore_backup
+    def restore_backup
       db_dump_zipfile = find_last_db_dump
       db_dump = unzip_db_dump(db_dump_zipfile)
-      return DbEksport.new.run_command("mysql -u#{db_user} #{db_name} #{db_pass_prase} < #{db_dump}")
+      run_command("mysql -u#{db_user} #{db_name} #{db_pass_prase} < #{db_dump}")
     end
 
-    def self.find_last_db_dump
+    def find_last_db_dump
       time_list = {}
-      Dir.entries(@@zip_path).each do |file|
+      Dir.entries(Udir::DB_DUMP_PATH).each do |file|
         if file =~ /db_dump_(.*)\.zip/
-          time_list[Time.parse($1)]= File.join(@@zip_path, file)
+          time_list[Time.parse($1)]= File.join(Udir::DB_DUMP_PATH, file)
         end
       end
 
       time_list[time_list.keys.sort.last]
     end
 
-    def self.unzip_db_dump(file)
+    def unzip_db_dump(file)
       require 'zip/zipfilesystem'
       dump_filename = File.join(Rails.root, "tmp", "restore.sql")
       
@@ -100,7 +86,5 @@ module Kompetansesok
       f.close
       dump_filename
     end
-
-    
   end
 end
