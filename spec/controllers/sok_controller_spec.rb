4958adca7aeed26b48a2e63370b82e20718d2105
aslakjo <aslakjo@bekk.no>
2009-02-03 11:14:11 +0100

    lagt til konvertering av csv til iso-8859-15 som er standard input til excel

--- spec/controllers/sok_controller_spec.rb
+++ spec/controllers/sok_controller_spec.rb
@@ -4,16 +4,31 @@ describe SokController do
 
 
   describe "responding to POST download_csv" do
-  
-    it "should generate csv and expose it as @content" do
-      generator = mock(Kompetansesok::CsvGenerator)
+    before :each do
+      @generator = mock(Kompetansesok::CsvGenerator)
       Kompetansesok::CsvGenerator.should_receive(:new).and_return(generator)
-      
-      generator.should_receive(:csv_for).with('some-uuids').and_return("as_csv æøå, –--")
+    end
+
+    it "should generate csv and expose it as @content" do
+      @generator.should_receive(:csv_for).with('some-uuids').and_return("as_csv, --")
+      post :download_csv, :uuids => 'some-uuids'
+      assigns[:content].should == c.iconv("as_csv, --")
+    end
+
+    it "should generate csv on ISO-8859-15" do
+      @generator.should_receive(:csv_for).with('some-uuids').and_return("æøå, --")
       post :download_csv, :uuids => 'some-uuids'
       c = Iconv.new('ISO-8859-15','UTF-8')
-      assigns[:content].should == c.iconv("as_csv æøå, ---")
+      assigns[:content].should == c.iconv("æøå, --")
       response.headers["type"].should == "text/csv; charset=ISO-8859-15"
+    end
+
+    it "should generate csv striped for special chars" do
+      special_to_replace_characters = {"–" => "-"}
+      @generator.should_receive(:csv_for).with('some-uuids').and_return(special_to_replace_characters.keys)
+      post :download_csv, :uuids => 'some-uuids'
+      c = Iconv.new('ISO-8859-15','UTF-8')
+      assigns[:content].should == c.iconv(special_to_replace_characters.values)
     end
   end
   
