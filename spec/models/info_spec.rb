d47224edfb6aacf88de991d4df391e462722a8c4
aslakjo <aslakjo@bekk.no>
2009-02-03 13:13:56 +0100

    skrev om tester for statistikk slik at jeg forsto dem, og at de ikke skal være sårbare for hvilken dag de er i dag.

--- spec/models/info_spec.rb
+++ spec/models/info_spec.rb
@@ -10,22 +10,18 @@ describe Info do
     Info.create!(@valid_attributes)
   end
   
-  describe "statistics" do
+  describe "static dates statistics" do
     
     before :each do
       today = Info.new(:created_at => Time.today)     
       start_of_this_week = Info.new(:created_at => Time.today.beginning_of_week)
       end_of_last_week = Info.new(:created_at => Time.today.beginning_of_week.yesterday)
       start_of_last_week = Info.new(:created_at => Time.today.beginning_of_week.yesterday.beginning_of_week)
-      start_of_this_month = Info.new(:created_at => Time.today.beginning_of_month)
-      last_month = Info.new(:created_at => Time.today.last_month)
-      start_of_last_month = Info.new(:created_at => Time.today.last_month.beginning_of_month)
       
-      infos = [today, start_of_this_week, end_of_last_week, start_of_last_week, start_of_this_month, last_month, start_of_last_month]
+      infos = [today, start_of_this_week, end_of_last_week, start_of_last_week]
       
       Info.should_receive(:find).with(:all, :conditions => "created_at >= #{Time.today.last_month.beginning_of_month.to_date}").and_return(infos)
     end
-    
     
     it "should have number of searches this week" do           
       Info.sok_statistikk[:denne_uke].should == 2
@@ -34,14 +30,25 @@ describe Info do
     it "should have number of searches last week" do
       Info.sok_statistikk[:forrige_uke].should == 2
     end
-    
-    it "should have number of searches this month" do
-      Info.sok_statistikk[:denne_maaned].should == 5
+  end
+
+  describe "dynamic dates statistics"  do
+    before :each do
+      last_month = Info.new(:created_at => Time.today.last_month) #forrige måned
+      start_of_last_month = Info.new(:created_at => Time.today.beginning_of_month.yesterday.beginning_of_month) #forrige måned
+      start_of_this_month = Info.new(:created_at => Time.today.beginning_of_month) #denne måned
+     infos = [last_month, start_of_this_month, start_of_last_month]
+
+      Info.should_receive(:find).with(:all, :conditions => "created_at >= #{Time.today.last_month.beginning_of_month.to_date}").and_return(infos)
     end
-    
+
+    it "should have number of searches this month" do
+      Info.sok_statistikk[:denne_maaned].should == 1
+    end
+
     it "should have number of searches last month" do
       Info.sok_statistikk[:forrige_maaned].should == 2
     end
-    
+
   end
 end
